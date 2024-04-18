f3542db3739120ff33a1e5033900be376d9c8358
rhyanndev <rhyannjosebezerra@gmail.com>
2024-04-17 21:36:20 -0300

    add: personalizando com CSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,23 +37,82 @@ function headerShadow(){
 
 /*-------------  TYPING EFFECT --------------*/
 
+var typingEffect = new Typed(".typedText", {
+    strings : ["Estudante", "Curioso", "Desenvolvedor"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 70,
+    backDelay : 2000
+})
 
-/*--------- ## -- SCROLL REVEAL ANIMATION -- ## ----------*/
 
-
-
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true     
+})
 /* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
 
-
-
-
-
-/* ---- PROJECT BOX ---- */
-
-
-
+/* -- PROJECT BOX -- */
+sr.reveal('.project-box',{interval: 200})
 /* -- HEADINGS -- */
+sr.reveal('.top-header',{})
+
+/* ----- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## ----- */
+/* -- ABOUT INFO & CONTACT INFO -- */
+const srLeft = ScrollReveal({
+origin: 'left',
+distance: '80px',
+duration: 2000,
+reset: true
+})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+
+/* -- ABOUT SKILLS & FORM BOX -- */
+const srRight = ScrollReveal({
+origin: 'right',
+distance: '80px',
+duration: 2000,
+reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+/* ----- CHANGE ACTIVE LINK ----- */
+
+const sections = document.querySelectorAll('section[id]')
+function scrollActive() {
+const scrollY = window.scrollY;
+sections.forEach(current =>{
+const sectionHeight = current.offsetHeight,
+    sectionTop = current.offsetTop - 50,
+  sectionId = current.getAttribute('id')
+if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) { 
+    document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+}  else {
+  document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+}
+})
+}
+window.addEventListener('scroll', scrollActive)
 
 
-
-/* ------- ## -- SCROLL REVEAL LEFT_RIGHT ANIMATION -- ## -------- */
+function downloadFile(url) {
+    var link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', '');
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
